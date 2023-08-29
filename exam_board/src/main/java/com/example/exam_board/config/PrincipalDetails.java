5c74ca66ad810e284d559e414fd1a2ac1081633b
brush-j <parefaro@gmail.com>
2023-08-29 10:43:44 +0900

    #9-OAuth2 인증 구현
    인증 구현 완료 및 테스트 완료

--- exam_board/src/main/java/com/example/exam_board/config/PrincipalDetails.java
+++ exam_board/src/main/java/com/example/exam_board/config/PrincipalDetails.java
@@ -3,21 +3,42 @@ package com.example.exam_board.config;
 import com.example.exam_board.entity.UserAccount;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails  implements UserDetails {
+public class PrincipalDetails  implements UserDetails, OAuth2User {
     private UserAccount userAccount;
 
-    public PrincipalDetails(UserAccount userAccount){
-        this.userAccount = userAccount;
+    private Map<String, Object> attributes;
+
+    public PrincipalDetails(UserAccount user, Map<String, Object> attributes) {
+        this.userAccount = user;
+        this.attributes = attributes;
+    }
+
+    public PrincipalDetails(UserAccount user) {
+        this.userAccount = user;
+    }
+
+    public UserAccount getUser() {
+        return userAccount;
+    }
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Collection<GrantedAuthority> collect = new ArrayList<>();
-        collect.add(()->{return userAccount.getRole();});
+        collect.add(() -> {
+            return userAccount.getRole();
+        });
         return collect;
     }
 
@@ -50,4 +71,11 @@ public class PrincipalDetails  implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    @Override
+    public String getName() {
+        String sub = attributes.get("id").toString();
+        return sub;
+    }
+
 }
